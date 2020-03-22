4931e2f5b3025a5d566cd560d51f7182d83e5a31
zhangrundong@meitq.com <1124028702@qq.com>
2020-03-22 15:30:43 +0800

    SSSS

--- demo-commons/demo-base-starter/src/main/java/com/copy/WebMvcConfig.java
+++ demo-commons/demo-base-starter/src/main/java/com/copy/WebMvcConfig.java
@@ -22,7 +22,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     }
 
     @Override
-    public void addInterceptors(InterceptorRegistry registry) {
+    public void  addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(callTimeLogInterceptor()).excludePathPatterns("/error");
     }
 
